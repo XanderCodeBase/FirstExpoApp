19a12867f2f8562e9d17d14422fa7e9c5e48de68
XanderCodeBase <github2@xinc.nl>
2026-05-09 18:50:11 +0200

    added SafeAreaProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import '../../styles/globals.css';
 
 import { Stack } from 'expo-router';
 import { View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import FooterMenu from '@/components/FooterMenu';
 import { AuthProvider } from '@/providers/AuthProvider';
@@ -9,14 +10,16 @@ import { AuthProvider } from '@/providers/AuthProvider';
 export default function RootLayout() {
     return (
         <AuthProvider>
-            <View className="flex-1 bg-slate-950">
-                <Stack screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="login" />
-                    <Stack.Screen name="(public)" />
-                    <Stack.Screen name="(auth)" />
-                </Stack>
-                <FooterMenu />
-            </View>
+            <SafeAreaProvider>
+                <View className="flex-1 bg-slate-950">
+                    <Stack screenOptions={{ headerShown: false }}>
+                        <Stack.Screen name="login" />
+                        <Stack.Screen name="(public)" />
+                        <Stack.Screen name="(auth)" />
+                    </Stack>
+                    <FooterMenu />
+                </View>
+            </SafeAreaProvider>
         </AuthProvider>
     );
 }
